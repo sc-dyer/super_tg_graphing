89ef88013a08a5a2a5b8278ec73d190ee7fd31b3
sc-dyer <sabastien.dyer@carleton.ca>
2019-07-22 15:23:05 -0400

    Added reading of PTt-path.txt to Model.py

--- Model.py
+++ Model.py
@@ -1,10 +1,13 @@
-#Class to open and manage an individual model 
+#Class to open and manage an individual model
 import csv
 import os
 
 class Model:
 	PATH_FILE_NAME = "PTt-path.txt" #File name where the PTt path is stored
 	GRT_FILE_PREF = "garnet_gen" #File prefix for the modelled compositions e.g "garnet_gen001a.txt"
+	TEMP_COL = 0
+	PRES_COL = 1
+	TIM_COL = 2
 
 	def __init__(self,dirIn,gen,trial):
 
@@ -17,15 +20,15 @@ class Model:
 			slash= "/"
 
 		#File paths for the PTt path and garnet composition
-		pathDir = dirIn + slash + PATH_FILE_NAME
-		grtDir = '%(directory)%(slash)%(fileName)%(generation)03d.txt' % \
-			{"directory":dirIn,"slash":slash,"fileName":GARNET_FILE_PREF,"generation":gen}
+		pathDir = dirIn + slash + self.PATH_FILE_NAME
+		grtDir = dirIn + slash + self.GRT_FILE_PREF + '{:03d}'.format(gen) + '.txt'
 
-		
+
+
 		try:
 			pathFile = open(pathDir, 'r')
 		except:
-			print("PTt-path.txt for trial: " + trial + " not found")
+			print("PTt-path.txt for trial: " + str(trial) + " not found")
 			return
 
 		self.trial = trial
@@ -33,12 +36,28 @@ class Model:
 		try:
 			grtFile = open(grtDir, 'r')
 		except:
-			print("Garnet generation: " + gen + " not found in trial: " + trial)
+			print("Garnet generation: " + str(gen) + " not found in trial: " + str(trial))
 			print("Please select different generation if you wish to include this trial")
 			return
 
 		self.gen = gen
-		
+
+
+		self.pressure = []
+		self.temperature = []
+		self.time = []
+
+		#Read the PTt path
+		line = pathFile.readline() #remove header
+		line = pathFile.readline()
+		while(len(line) > 0):
+			items = [value.strip() for value in line.split()]
+			self.temperature.append(float(items[self.TEMP_COL]))
+			self.pressure.append(float(items[self.PRES_COL]))
+			self.time.append(float(items[self.TIM_COL]))
+			line = pathFile.readline()
+
+
 
 
 
@@ -48,7 +67,7 @@ class Model:
 		# self.temperature
 		# self.time
 		# self.x
-		# self.mn 
-		# self.mg 
+		# self.mn
+		# self.mg
 		# self.fe
-		# self.ca 
+		# self.ca
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-#This is a program to plot the output of SUPER_TG and compare it to 
+#This is a program to plot the output of SUPER_TG and compare it to
 #traverses measured in real samples
 
 
-
+import Model
 import os
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
 
+
+
+fileIn = input('Enter the directory where the SUPER_TG trials are stored: ')
+fileIn = fileIn.strip()
+fileIn = fileIn.strip('"')
+
+
+#fileOut = input('Enter the desired directory for the output files to be saved (WARNING: THIS WILL OVERWRITE FILES OF THE SAME NAME, SAVE TO NEW FOLDER IF YOU DONT WANT THIS TO HAPPEN): ')
+#fileOut = fileOut.strip()
+#fileOut = fileOut.strip('"')
+
+#testModel = Model.Model(fileIn, 1, 1)
+#print(testModel.trial)
+#print(testModel.gen)
+#print(testModel.temperature)
